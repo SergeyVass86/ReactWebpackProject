d65edec8b0da71ab9621963d5eb5cb044e8d5c3a
SerVass <test@test.com>
2019-08-15 13:33:12 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import Users from './containers/Users';
 import asyncComponent from './hoc/asyncComponent';
 
 const AsyncPizza = asyncComponent(() => {
-    return import('./containers/Pizza');
-})
+    return import('./containers/Pizza.js');
+});
 
 class App extends Component {
-
-    render() {
+    render () {
         return (
             <div>
                 <div>
-                    <Link to="/">Users</Link>
-                    <Link to="/pizza">Pizza</Link>
+                    <Link to="/">Users</Link> | <Link to="/pizza">Pizza</Link>
                 </div>
                 <div>
                     <Route path="/" exact component={Users} />
-                    <Route path="/pizza" exact component={AsyncPizza} />
+                    <Route path="/pizza" component={AsyncPizza} />
                 </div>
             </div>
         );
